803b4064f442807ca1d852477d2b6439e6f2d895
Cihat Keskin <chtkskns37@gmail.com>
2022-09-20 17:05:53 +0300

    ifelse folder added and changed

---
+++ If-Else/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace If_Else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 11)
+                Console.WriteLine("Günaydin!");
+            else if (time <= 18)
+                Console.WriteLine("Iyi gunler!");
+            else
+                Console.WriteLine("Iyi geceler!");
+
+            string sonuc = time<=18 ? "Iyi gunler!" : "Iyi geceler!";
+
+            Console.WriteLine(sonuc);
+
+            sonuc = time>= 6 && time<11 ? "Gunaydin!" : time <= 18 ? "Iyi gunler!" : "Iyi geceler!";
+
+            Console.WriteLine(sonuc);
+        }
+    }
+}
