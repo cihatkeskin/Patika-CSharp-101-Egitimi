b296c09b68e64d1763011636c1b43fa0629a7dfd
Cihat Keskin <chtkskns37@gmail.com>
2022-09-29 12:11:41 +0300

    metodoverloading folder added and changed

---
+++ MetodOverloading/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace MetodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Out Parametreler
+            string sayi = "999";
+
+            bool sonuc = int.TryParse(sayi, out int outSayi);
+
+            if (sonuc)
+            {
+                Console.WriteLine("Basarili!");
+                Console.WriteLine(outSayi);
+            }
+
+            else
+            {
+                Console.WriteLine("Basarisiz!");
+            }
+
+            Metodlar instance = new Metodlar();
+            instance.Topla(4, 5, out int ToplamSonucu);
+            Console.WriteLine(ToplamSonucu);
+
+
+            // Metod Overloading - Aşırı Yükleme
+
+            int ifade = 999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("Cihat", " Keskin");
+
+
+            // Metod İmzası
+            // metodAdi + parametre sayisi + parametre tipi -> bunların üçü aynı olmamalı.
+        }
+    }
+
+    class Metodlar
+    {
+        public void Topla(int a, int b, out int toplam)
+        {
+            toplam = a + b;
+        }
+
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public void EkranaYazdir(string veri1, string veri2)
+        {
+            Console.WriteLine(veri1 + veri2);
+        }
+    }
+}
